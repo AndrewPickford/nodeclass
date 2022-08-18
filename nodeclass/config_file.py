9fcb85059e5b8933f946f0dcdca596f4219ce5d8
Andrew Pickford <anpickford@googlemail.com>
2022-08-18 16:13:28 +0200

    Add type hints to config file loader methods

--- nodeclass/config_file.py
+++ nodeclass/config_file.py
@@ -4,14 +4,20 @@ import yaml
 from .constants import CONFIG_FILE_NAME, CONFIG_FILE_SEARCH_PATH
 from .exceptions import ConfigFileParseError, NoConfigFile
 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Any, Dict, List, Optional, Tuple, Union
+    ConfigData = Dict[str, Any]
+
 SafeLoader = yaml.CSafeLoader if yaml.__with_libyaml__ else yaml.SafeLoader
 
-def split_settings_location(config):
+
+def split_settings_location(config: 'ConfigData') -> 'Tuple[ConfigData, Union[ConfigData, None]]':
     settings = { key: value for key, value in config.items() if key != 'uri' }
     uri = config.get('uri', None)
     return settings, uri
 
-def load_config_file(filename = None, search_path = None):
+def load_config_file(filename: 'Optional[str]' = None, search_path: 'Optional[List[str]]' = None) -> 'Tuple[ConfigData, Union[ConfigData, None], str]':
     filename = filename or CONFIG_FILE_NAME
     search_path = search_path or CONFIG_FILE_SEARCH_PATH
 
