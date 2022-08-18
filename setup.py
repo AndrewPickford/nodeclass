3f17810729d8b6ab06f1ae503c681acc761d9147
Andrew Pickford <anpickford@googlemail.com>
2022-08-18 16:16:38 +0200

    Add different rpm requirements for el7 and el8

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
+import distro
+import platform
 from setuptools import setup, find_packages
 from nodeclass.version import DESCRIPTION, VERSION
+
+el7 = {
+    'requires': [
+        'python36-pyparsing',
+    ],
+}
+
+el8 = {
+    'requires': [
+        'python3-pyparsing',
+    ],
+}
+
+options = { 'bdist_rpm': {} }
+if platform.system() == 'Linux':
+    if distro.id() == 'rhel' or 'rhel' in distro.like().split():
+        if distro.major_version == '7':
+            options['bdist_rpm']['requires'] = el7['requires']
+        else:
+            options['bdist_rpm']['requires'] = el8['requires']
 
 setup(
       packages=find_packages(exclude=['tests']),
