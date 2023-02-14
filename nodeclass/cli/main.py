728a66f3c46dadd22f5a3247e371ede9379912f6
Andrew Pickford <anpickford@googlemail.com>
2023-02-14 09:37:11 +0100

    Use a custom yaml representer for multiline strings
    Change the output of multiline strings from:
        test: 'line 1
    
          line 2
    
          line3
    
          '
    
    to:
        test: |
          line 1
          line 2
          line 3
    
    This does not change the data passed into salt, this only changes
    the displayed format for multiline strings.

--- nodeclass/cli/main.py
+++ nodeclass/cli/main.py
@@ -1,4 +1,5 @@
 import sys
+import yaml
 from ..exceptions import NodeclassError
 from .parser import make_parser
 from .command_inventory import command_inventory
@@ -12,6 +13,18 @@ COMMANDS = {
     'param': command_param,
     'version': command_version,
 }
+
+def str_presenter(dumper, data):
+    '''
+        Configure yaml for dumping multiline strings
+        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+    '''
+    if data.count('\n') > 0:  # check for multiline string
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+yaml.add_representer(str, str_presenter)
+yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
 
 def main():
     parser = make_parser()
